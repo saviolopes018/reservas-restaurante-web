c4adb52eae5376d66dea66e5033b8fbd4320d6bc
Sávio Lopes <savio.lopes404@gmail.com>
2021-02-09 22:49:28 -0300

    Organização das pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './landing-page/home/home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { TopoComponent } from './landing-page/topo/topo.component';
+import { QuemSomosComponent } from './landing-page/quem-somos/quem-somos.component';
+import { FeedBackComponent } from './landing-page/feed-back/feed-back.component';
+import { ContatoComponent } from './landing-page/contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopoComponent,
+    QuemSomosComponent,
+    FeedBackComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
